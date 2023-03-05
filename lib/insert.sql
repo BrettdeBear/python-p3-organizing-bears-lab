007d07f13e20d5285f2fc997de0eddfdf8f42708
Brett de Bear <bkdebear@gmail.com>
2023-03-05 14:09:07 -0800

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Hungry", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 8, "M", "Brown", "Rambunctious", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 4, "F", "Black", "Sassy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 9, "F", "Brown", "Bossy", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 4, "F", "White", "Silly", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 8, "F", "Black", "Cranky", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 6, "F", "White", "Chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 3, "F", "Brown", "Sweet", 1);
